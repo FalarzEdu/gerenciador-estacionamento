1da0c81263a1d99222836c38e89867b7bfdb9b9a
Eduardo Falarz <eduardofalarz23@gmail.com>
2023-08-20 22:49:18 -0300

    Primeiro commit

---
+++ tailwind.config.js
@@ -0,0 +1,21 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./src/**/*.{html,js}'],
+  theme: {
+    extend: {
+      colors: {
+        green: {
+          500: '#22A62E'
+        },
+        blue: {
+          700: '#1A5B8D'
+        },
+      },
+      fontFamily: {
+        'comic': ['Comic Neue']
+      }
+    },
+  },
+  plugins: [],
+}
+
